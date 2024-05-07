b51216eaa8413f4ae7e3710dde24fbf8cef0740e
Roberto <robertgianocca@outlook.comm>
2024-05-07 23:52:51 +0200

    Clean typescript's errors

--- src/components/Portfolio/PortfolioCategoryCard/PortfolioCategoryCard.tsx
+++ src/components/Portfolio/PortfolioCategoryCard/PortfolioCategoryCard.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
-
-const PortfolioCategoryCard = ({ item, portfolioData }) => {
+interface PortfolioCategoryCardProps {
+  item: any;
+  portfolioData: any;
+}
+const PortfolioCategoryCard = ({ item, portfolioData }: PortfolioCategoryCardProps) => {
   return (
     // <Link href={`/${item}`}>
     <Link href={item.includes(" ") ? item.replace(" ", "-") : `/${item}`}>
       <div className="w-full h-full relative flex flex-col p-8 bg-zinc-600 hover:bg-red-900 text-slate-200">
         <h1 className="text-4xl pb-4">{item}</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book.
+          a galley of type and scrambled it to make a type specimen book.`}
         </p>
         <div className="">
           {/* <Image
