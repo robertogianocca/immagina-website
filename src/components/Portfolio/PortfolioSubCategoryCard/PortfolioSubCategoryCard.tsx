d3da86fb89c10a5a3e52bdb6611c6ab643b6cee1
Roberto <robertgianocca@outlook.comm>
2024-05-07 23:56:19 +0200

    fix syntax issue

--- src/components/Portfolio/PortfolioSubCategoryCard/PortfolioSubCategoryCard.tsx
+++ src/components/Portfolio/PortfolioSubCategoryCard/PortfolioSubCategoryCard.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
-
+interface PortfolioCategoryCardProps {
+  item: any;
+  categoriesFromPath: any;
+}
 const PortfolioSubCategoryCard = ({
   item,
   // portfolioData
   // currentItem,
   categoriesFromPath,
-  // updatePortfolioData,
-}) => {
+}: // updatePortfolioData,
+PortfolioCategoryCardProps) => {
   // const imageCover = currentItem.pictures[0].url;
   // When we have categories like "finzi pasca"
   return (
@@ -20,9 +23,9 @@ const PortfolioSubCategoryCard = ({
         <div className="w-full h-full relative flex flex-col p-8 bg-zinc-600 hover:bg-red-900 text-slate-200">
           <h1 className="text-4xl pb-4">{item}</h1>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen book.
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book.`}
           </p>
         </div>
       </Link>
