9155edef85cfb34be800f747bac72d3dad40539a
Roberto <robertgianocca@outlook.comm>
2024-09-06 11:41:33 +0200

    Red button animation and color.

--- src/components/Buttons/RedButton.tsx
+++ src/components/Buttons/RedButton.tsx
@@ -12,7 +12,7 @@ export default function RedButton({ children, addClass, onClick }: Readonly<Butt
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.9 }}
       transition={{ duration: 0.5 }}
-      className={`bg-red-600 text-slate-50 flex items-center justify-center ${addClass} hover:bg-gradient-to-tr hover:from-red-700 hover:to-red-600`}
+      className={`bg-red-600 text-slate-50 flex items-center justify-center ${addClass} hover:bg-gradient-to-tl hover:from-red-700 hover:to-red-600`}
       onClick={onClick}
     >
       {children}
