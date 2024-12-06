e7eced203de2414d59db5a59cc28118fb67624e1
Roberto <robertogianocca@outlook.com>
2024-12-06 11:59:37 +0100

    Team section.

--- src/components/Sections/TeamSection/TeamSection.tsx
+++ src/components/Sections/TeamSection/TeamSection.tsx
@@ -1,41 +1,28 @@
-import Image from "next/image";
-import imgOne from "/public/images/samples/01.jpg";
+import portraitRobertoGianocca from "/public/images/team/roberto-gianocca.jpg";
+import portraitAdrianoHeitmann from "/public/images/team/adriano-heitmann.jpg";
+import TeamCard from "@/components/Sections/TeamSection/TeamCard/TeamCard";
+
+const teamList = [
+  {
+    name: "Adriano Heitmann",
+    description: "Fondatore. Fotografo e art director.",
+    picture: portraitAdrianoHeitmann,
+  },
+  {
+    name: "Roberto Gianocca",
+    description: "Regista, cameraman, fotografo, grafico, webdesigner.",
+    picture: portraitRobertoGianocca,
+  },
+];
+
+const mappedTeam = teamList.map((item, index) => (
+  <TeamCard key={index} name={item.name} description={item.description} picture={item.picture} />
+));
+
 export default function TeamSection() {
   return (
-    <div className="grid grid-cols-6 gap-6">
-      <div className="flex flex-col">
-        <div className="w-full h-[300px] bg-slate-400"></div>
-      </div>
-      <div className="">
-        <h2 className="text-base">Adriano Heitmann</h2>
-        <p>
-          {
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-          }
-        </p>
-      </div>
-      <div className="flex flex-col">
-        <div className="w-full h-[300px] bg-slate-400"></div>
-      </div>
-      <div className="">
-        <h2 className="text-base">Roberto Gianocca</h2>
-        <p>
-          {
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-          }
-        </p>
-      </div>
-      <div className="flex flex-col">
-        <div className="w-full h-[300px] bg-slate-400"></div>
-      </div>
-      <div className="">
-        <h2 className="text-base">Omar Beltraminelli</h2>
-        <p>
-          {
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-          }
-        </p>
-      </div>
+    <div className="flex flex-col xl:grid md:grid-cols-2 2xl:grid-cols-3 gap-y-10 xl:gap-6">
+      {mappedTeam}
     </div>
   );
 }
